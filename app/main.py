e16ad5a767e2d02fd9373b57d07ddc886aa97081
Mohit Patel <mohitpatel.pbh2003@gmail.com>
2026-07-21 15:40:39 +0530

    Fix CORS error by setting allow_origin_regex='.*' in CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -149,7 +149,7 @@ async def global_exception_handler(request, exc):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origin_regex=".*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
